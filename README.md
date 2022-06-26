e1d7670101993758a42ccada632ac43e6307aa70
Ahmed Elaidy <alaidy.y123@gmail.com>
2022-06-26 16:34:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Main Text 
+<hr>
+Tours readme file
+<hr>
+## The languages that i used:
+<hr>
+- REACT <br>
+- JAVASCRIPT <br>
+- HTML <br>
+- CSS <br>
+<hr>
+## The Functionalities:
+<hr>
+- SideBar is a React project that has a one-button that has a function.<br>
+- When the button is been clicked it runs a function that adds a class (show-links) to the sidebar which makes the SideBar appear.<br>
+- if the SideBar already have (show-links) class then the button removes the class which makes the SideBar disappears.<br>
+<hr>
---
+++ src/context.js
@@ -0,0 +1,33 @@
+import React , {useContext,useState} from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+  const [isSidebarOpen , setIsSidebarOpen] = useState(false)
+  const [isModalOpen , setIsModalOpen] = useState(false)
+
+  const openSideBar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const closeSideBar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <AppContext.Provider value={{isSidebarOpen, isModalOpen, openSideBar , closeSideBar,openModal ,closeModal }}>{children}</AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export {AppProvider}
